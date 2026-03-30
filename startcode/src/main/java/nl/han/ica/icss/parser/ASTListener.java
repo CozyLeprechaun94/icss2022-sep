65d156266b35ff9bbddbca16d97f46e33e00ffb3
CozyLeprechaun94 <44181698+CozyLeprechaun94@users.noreply.github.com>
2026-03-30 14:31:11 +0200

    reformat goes brrr

--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -19,116 +19,113 @@ import nl.han.ica.icss.parser.ICSSBaseListener;
  */
 public class ASTListener extends ICSSBaseListener {
 
-	//Accumulator attributes:
-	private final AST ast;
+    //Accumulator attributes:
+    private final AST ast;
 
-	//Use this to keep track of the parent nodes when recursively traversing the ast
-	private IHANStack<ASTNode> currentContainer;
+    //Use this to keep track of the parent nodes when recursively traversing the ast
+    private IHANStack<ASTNode> currentContainer;
 
-	public ASTListener(AST ast) {
-		this.ast = ast;
-		currentContainer = new HANStack<>();
-	}
+    public ASTListener(AST ast) {
+        this.ast = ast;
+        currentContainer = new HANStack<>();
+    }
 
-	public ASTListener() {
-		ast = new AST();
-		currentContainer = new HANStack<>();
-		currentContainer.push(ast.root);
-	}
+    public ASTListener() {
+        ast = new AST();
+        currentContainer = new HANStack<>();
+        currentContainer.push(ast.root);
+    }
 
-	public AST getAST() {
-		return ast;
-	}
+    public AST getAST() {
+        return ast;
+    }
 
-	@Override
-	public void enterVariableAssignment(ICSSParser.VariableAssignmentContext ctx) {
-		VariableAssignment varAssignment = new VariableAssignment();
-		currentContainer.peek().addChild(varAssignment);
-		currentContainer.push(varAssignment);
-	}
+    @Override
+    public void enterVariableAssignment(ICSSParser.VariableAssignmentContext ctx) {
+        VariableAssignment varAssignment = new VariableAssignment();
+        currentContainer.peek().addChild(varAssignment);
+        currentContainer.push(varAssignment);
+    }
 
-	@Override
-	public void exitVariableAssignment(ICSSParser.VariableAssignmentContext ctx) {
-		currentContainer.pop();
-	}
+    @Override
+    public void exitVariableAssignment(ICSSParser.VariableAssignmentContext ctx) {
+        currentContainer.pop();
+    }
 
-	@Override
-	public void enterVariableReference(ICSSParser.VariableReferenceContext ctx) {
-		currentContainer.peek().addChild(new VariableReference(ctx.getText()));
-	}
+    @Override
+    public void enterVariableReference(ICSSParser.VariableReferenceContext ctx) {
+        currentContainer.peek().addChild(new VariableReference(ctx.getText()));
+    }
 
-	@Override
-	public void enterStylerule(ICSSParser.StyleruleContext ctx) {
-		Stylerule stylerule = new Stylerule();
-		currentContainer.peek().addChild(stylerule);
-		currentContainer.push(stylerule);
-	}
+    @Override
+    public void enterStylerule(ICSSParser.StyleruleContext ctx) {
+        Stylerule stylerule = new Stylerule();
+        currentContainer.peek().addChild(stylerule);
+        currentContainer.push(stylerule);
+    }
 
-	@Override
-	public void exitStylerule(ICSSParser.StyleruleContext ctx) {
-		currentContainer.pop();
-	}
+    @Override
+    public void exitStylerule(ICSSParser.StyleruleContext ctx) {
+        currentContainer.pop();
+    }
 
-	@Override
-	public void enterSelector(ICSSParser.SelectorContext ctx) {
-		Selector selector;
-		if (ctx.CLASS_IDENT() != null) {
-			selector = new ClassSelector(ctx.CLASS_IDENT().getText());
-		} else if (ctx.ID_IDENT() != null) {
-			selector = new IdSelector(ctx.ID_IDENT().getText());
-		} else {
-			selector = new TagSelector(ctx.LOWER_IDENT().getText());
-		}
-		currentContainer.peek().addChild(selector);
-	}
+    @Override
+    public void enterSelector(ICSSParser.SelectorContext ctx) {
+        Selector selector;
+        if (ctx.CLASS_IDENT() != null) {
+            selector = new ClassSelector(ctx.CLASS_IDENT().getText());
+        } else if (ctx.ID_IDENT() != null) {
+            selector = new IdSelector(ctx.ID_IDENT().getText());
+        } else {
+            selector = new TagSelector(ctx.LOWER_IDENT().getText());
+        }
+        currentContainer.peek().addChild(selector);
+    }
 
-	@Override
-	public void enterExpression(ICSSParser.ExpressionContext ctx) {
-		Expression expression;
-		if (ctx.op.getType() == ICSSParser.PLUS) {
-			expression = new AddOperation();
-		} else if (ctx.op.getType() == ICSSParser.MIN) {
-			expression = new SubtractOperation();
-		} else if (ctx.op.getType() == ICSSParser.MUL) {
-			expression = new MultiplyOperation();
-		} else {
-			throw new RuntimeException("Unrecognized expression operator: " + ctx.getText());
-		}
-		currentContainer.peek().addChild(expression);
-		currentContainer.push(expression);
-	}
+    @Override
+    public void enterExpression(ICSSParser.ExpressionContext ctx) {
+        Expression expression;
+        if (ctx.op != null) {
+            if (ctx.op.getType() == ICSSParser.PLUS) {
+                expression = new AddOperation();
+            } else if (ctx.op.getType() == ICSSParser.MIN) {
+                expression = new SubtractOperation();
+            } else if (ctx.op.getType() == ICSSParser.MUL) {
+                expression = new MultiplyOperation();
+            } else {
+                throw new RuntimeException("Unrecognized expression operator: " + ctx.getText());
+            }
+            currentContainer.peek().addChild(expression);
+            currentContainer.push(expression);
+        }
+    }
 
-	@Override
-	public void exitExpression(ICSSParser.ExpressionContext ctx) {
-		currentContainer.pop();
-	}
+    @Override
+    public void enterDeclaration(ICSSParser.DeclarationContext ctx) {
+        Declaration declaration = new Declaration();
+        declaration.property = new PropertyName(ctx.propertyName().getText());
+        currentContainer.peek().addChild(declaration);
+        currentContainer.push(declaration);
+    }
 
-	@Override
-	public void enterDeclaration(ICSSParser.DeclarationContext ctx) {
-		Declaration declaration = new Declaration();
-		declaration.property = new PropertyName(ctx.propertyName().getText());
-		currentContainer.peek().addChild(declaration);
-		currentContainer.push(declaration);
-	}
-
-	@Override
-	public void enterLiteral(ICSSParser.LiteralContext ctx) {
-		Literal literal;
-		if (ctx.COLOR() != null) {
-			literal = new ColorLiteral(ctx.getText());
-		} else if (ctx.PIXELSIZE() != null) {
-			literal = new PixelLiteral(Integer.parseInt(ctx.getText().replace("px", "")));
-		} else if (ctx.TRUE() != null) {
-			literal = new BoolLiteral(ctx.getText());
-		} else if (ctx.FALSE() != null) {
-			literal = new BoolLiteral(ctx.getText());
-		} else if (ctx.SCALAR() != null) {
-			literal = new ScalarLiteral(Integer.parseInt(ctx.getText()));
-		} else if (ctx.PERCENTAGE() != null) {
-			literal = new PercentageLiteral(Integer.parseInt(ctx.getText().replace("%", "")));
-		} else {
-			throw new RuntimeException("Unrecognized literal: " + ctx.getText());
-		}
-		currentContainer.peek().addChild(literal);
-	}
+    @Override
+    public void enterLiteral(ICSSParser.LiteralContext ctx) {
+        Literal literal;
+        if (ctx.COLOR() != null) {
+            literal = new ColorLiteral(ctx.getText());
+        } else if (ctx.PIXELSIZE() != null) {
+            literal = new PixelLiteral(Integer.parseInt(ctx.getText().replace("px", "")));
+        } else if (ctx.TRUE() != null) {
+            literal = new BoolLiteral(ctx.getText());
+        } else if (ctx.FALSE() != null) {
+            literal = new BoolLiteral(ctx.getText());
+        } else if (ctx.SCALAR() != null) {
+            literal = new ScalarLiteral(Integer.parseInt(ctx.getText()));
+        } else if (ctx.PERCENTAGE() != null) {
+            literal = new PercentageLiteral(Integer.parseInt(ctx.getText().replace("%", "")));
+        } else {
+            throw new RuntimeException("Unrecognized literal: " + ctx.getText());
+        }
+        currentContainer.peek().addChild(literal);
+    }
 }
