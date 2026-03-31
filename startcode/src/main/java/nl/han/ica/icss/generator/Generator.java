ef721d428b37d334ec1f731c8848aef60acb632c
CozyLeprechaun94 <44181698+CozyLeprechaun94@users.noreply.github.com>
2026-03-31 23:17:18 +0200

    creatingstyle rule

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -3,19 +3,31 @@ package nl.han.ica.icss.generator;
 
 import nl.han.ica.icss.ast.AST;
 import nl.han.ica.icss.ast.ASTNode;
+import nl.han.ica.icss.ast.Selector;
 import nl.han.ica.icss.ast.Stylerule;
 
 public class Generator {
 
-	public String generate(AST ast) {
-		StringBuilder result = new StringBuilder(); // Maak een nieuwe StringBuilder aan, omdat String niet veranderbaar is.
+    public String generate(AST ast) {
+        StringBuilder result = new StringBuilder(); // Maak een nieuwe StringBuilder aan, omdat String niet veranderbaar is.
 
-		for (ASTNode child : ast.root.getChildren()){
-			if (child instanceof Stylerule stylerule) {
-				// Er moet wat gebeuren
-			}
-		}
+        for (ASTNode child : ast.root.getChildren()) {
+            if (child instanceof Stylerule stylerule) {
+                createStylerule(stylerule, result);
+            }
+        }
         return result.toString(); //Zet om naar String
-	}
+    }
 
+    private StringBuilder createStylerule(Stylerule stylerule, StringBuilder result) {
+        result.append(stylerule.selectors.toString());
+        for (ASTNode node : stylerule.body) {
+            if(node instanceof Selector selector) {
+                result.append(selector.toString());
+            }
+        }
+
+        result.toString();
+        return result;
+    }
 }
