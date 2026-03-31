1aa2d1908ff5709d5534b5589dfc6eed88847f49
CozyLeprechaun94 <44181698+CozyLeprechaun94@users.noreply.github.com>
2026-03-31 10:48:22 +0200

    calculate value and add to list

--- startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -3,15 +3,13 @@ package nl.han.ica.icss.transforms;
 import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
-import nl.han.ica.icss.ast.literals.PercentageLiteral;
-import nl.han.ica.icss.ast.literals.PixelLiteral;
-import nl.han.ica.icss.ast.literals.ScalarLiteral;
+import nl.han.ica.icss.ast.literals.*;
 import nl.han.ica.icss.ast.operations.AddOperation;
 import nl.han.ica.icss.ast.operations.MultiplyOperation;
 import nl.han.ica.icss.ast.operations.SubtractOperation;
+import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 
 public class Evaluator implements Transform {
 
@@ -25,5 +23,30 @@ public class Evaluator implements Transform {
     public void apply(AST ast) {
         variableValues = new HANLinkedList<>();
 
+        for (ASTNode child : ast.root.getChildren()) {
+            if (child instanceof VariableAssignment va) {
+                applyVariableAssignment(va);
+            } else if (child instanceof Stylerule rule) {
+                applyStylerule(rule);
+            }
+        }
+
+        variableValues.removeFirst();
+    }
+
+    private void applyVariableAssignment(VariableAssignment variableAssignment){
+        Literal value = valueExpression(variableAssignment.expression);
+        variableValues.getFirst().put(variableAssignment.name.name, value);
+    }
+
+    private void applyStylerule(Stylerule stylerule){
+        // empty
+    }
+
+
+    private Literal valueExpression(Expression expression) {
+        if (expression instanceof Literal literal) return literal;
+
+        return null;
     }
 }
