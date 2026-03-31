2c6066830e098382c65ef7a3f9fcfa84f1fb4625
CozyLeprechaun94 <44181698+CozyLeprechaun94@users.noreply.github.com>
2026-03-31 23:00:59 +0200

    generator start code

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -2,14 +2,20 @@ package nl.han.ica.icss.generator;
 
 
 import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.ASTNode;
+import nl.han.ica.icss.ast.Stylerule;
 
 public class Generator {
 
 	public String generate(AST ast) {
-        return "";
+		StringBuilder result = new StringBuilder(); // Maak een nieuwe StringBuilder aan, omdat String niet veranderbaar is.
 
-
+		for (ASTNode child : ast.root.getChildren()){
+			if (child instanceof Stylerule stylerule) {
+				// Er moet wat gebeuren
+			}
+		}
+        return result.toString(); //Zet om naar String
 	}
 
-	
 }
