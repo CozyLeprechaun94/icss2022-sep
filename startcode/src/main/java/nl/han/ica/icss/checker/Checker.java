c7a9df77dd16576d8e337d1cff5e3027ab7858d6
CozyLeprechaun94 <44181698+CozyLeprechaun94@users.noreply.github.com>
2026-03-31 09:43:54 +0200

    base checker with variableassignment

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -1,11 +1,13 @@
 package nl.han.ica.icss.checker;
 
+import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
-import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.*;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
-
+import java.util.List;
 
 
 public class Checker {
@@ -13,9 +15,50 @@ public class Checker {
     private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
 
     public void check(AST ast) {
-        // variableTypes = new HANLinkedList<>();
-
+        variableTypes = new HANLinkedList<>();
+        check(ast.root);
     }
 
-    
+    private void check(Stylesheet stylesheet) {
+        variableTypes.addFirst(new HashMap<>());
+
+        for (ASTNode child : stylesheet.getChildren()) {
+            if (child instanceof VariableAssignment variableAssignment) {
+                controlVariableAssignment(variableAssignment);
+            } else if (child instanceof Stylerule styleRule) {
+                controlStylerule(styleRule);
+            }
+        }
+
+        variableTypes.removeFirst();
+    }
+
+    private void controlStylerule(Stylerule rule) {
+        variableTypes.addFirst(new HashMap<>());
+        controlBody(rule.body);
+        variableTypes.removeFirst();
+    }
+
+    private void controlBody(List<ASTNode> body) {
+        for (ASTNode node : body) {
+            if (node instanceof VariableAssignment va) {
+                controlVariableAssignment(va);
+            }
+        }
+    }
+
+    private void controlVariableAssignment(VariableAssignment variableAssignment) {
+        ExpressionType expressionType = getExpressionType(variableAssignment.expression);
+        variableTypes.getFirst().put(variableAssignment.name.name, expressionType);
+    }
+
+    private ExpressionType getExpressionType(Expression expression) {
+        if (expression instanceof BoolLiteral) return ExpressionType.BOOL;
+        if (expression instanceof ColorLiteral) return ExpressionType.COLOR;
+        if (expression instanceof PixelLiteral) return ExpressionType.PIXEL;
+        if (expression instanceof PercentageLiteral) return ExpressionType.PERCENTAGE;
+        if (expression instanceof ScalarLiteral) return ExpressionType.SCALAR;
+
+        return ExpressionType.UNDEFINED;
+    }
 }
