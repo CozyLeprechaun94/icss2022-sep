6a31f86cced73db8d56ed6c1e72599cb19bbb403
CozyLeprechaun94 <44181698+CozyLeprechaun94@users.noreply.github.com>
2026-03-31 23:39:33 +0200

    Finished generator

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -2,6 +2,7 @@ package nl.han.ica.icss.generator;
 
 
 import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.*;
 
 public class Generator {
 
@@ -18,10 +19,10 @@ public class Generator {
 
     private void createStylerule(Stylerule stylerule, StringBuilder result) {
         result.append(stylerule.selectors.get(0).toString()); // Selector naar string
-        result.append("{\n"); // Na Selector open curly bracet
+        result.append(" {\n"); // Na Selector open curly bracet
 
         for (ASTNode node : stylerule.body) {
-            if(node instanceof Declaration declaration) {
+            if (node instanceof Declaration declaration) {
                 createDeclaration(declaration, result);
             }
         }
@@ -30,10 +31,23 @@ public class Generator {
     }
 
     private void createDeclaration(Declaration declaration, StringBuilder result) {
-        result.append(declaration.property.name) // Iets van background-color
+        result.append("  ") //2 spaties
+                .append(declaration.property.name) // Iets van background-color
                 .append(": ") //Na het definiëren van de property name background-color:
-                .append(declaration.expression) // Voeg expression toe
+                .append(getLiteralValue(declaration.expression)) // Voeg expression toe
                 .append(";\n"); // Sluit de declaration
 
+    }
+
+    private String getLiteralValue(Expression expression) {
+        if (expression instanceof PixelLiteral pixelLiteral) {
+            return pixelLiteral.value + "px"; //Return px bij PixelLiteral
+        } else if (expression instanceof PercentageLiteral percentageLiteral){
+            return percentageLiteral.value + "%";
+        } else if (expression instanceof ColorLiteral colorLiteral) {
+            return colorLiteral.value; //return value van color ex. #ffffff
+        }
+
+        return ""; //return niks
     }
 }
